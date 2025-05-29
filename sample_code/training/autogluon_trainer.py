795cc506edbc9bef827cedb57f89f883578c3a08
qqjk123 <zfnb1997@gmail.com>
2025-05-29 00:42:09 -0400

    Create autogluon_trainer.py

---
+++ sample_code/training/autogluon_trainer.py
@@ -0,0 +1,43 @@
+# Dice Enhanced
+from autogluon.multimodal import MultiModalPredictor
+
+hyperparameters = {
+    "optimization": {
+        # 混合精度训练，节省显存、加速
+        "fp16": True,              
+        # 每张卡 batch size，4090 24 GB 可以试 8 或 16，根据图像大小调小／调大
+        "per_gpu_batch_size": 2,   
+        # 小数据集适当增加epoch次数
+        "max_epochs": 10,          
+        # 学习率微调：小数据集、少样本，用小一点的 LR 更稳定
+        "lr": 1e-4,                
+        # L2 正则，防止过拟合
+        "weight_decay": 1e-4,      
+        # 梯度累积：如果想试更大等效 batch，再叠几步
+        "gradient_accumulation_steps": 1,
+        # 早停配置，val 不提升就停
+        "early_stop": {"monitor": "val_dice", "patience": 3},
+        # 数据加载预取
+        "num_workers": 18,
+        "loss_function": "dice"
+    },
+    "model.sam.checkpoint_name": "facebook/sam-vit-base"
+}
+
+predictor = MultiModalPredictor(
+    problem_type="semantic_segmentation",
+    label="label",
+    path="automm/automm_dataset002",
+    presets="medium_quality",   
+    eval_metric="dice",              # 训练/调参时的评估指标
+    validation_metric="dice",      # 可选 medium_quality / high_quality
+    hyperparameters=hyperparameters,
+)
+
+print("Validation metric is:", predictor._learner._validation_metric_name)
+
+predictor.fit(
+    train_data="/workspace/autogluon_original_Dataset002_BraTS2023_SEG/train.csv",
+    tuning_data="/workspace/autogluon_original_Dataset002_BraTS2023_SEG/val_reduced.csv",
+    time_limit=14400,                 # 4 小时
+)
